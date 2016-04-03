5c8bee825e9c3126d382e3077a722c897c636614
smosker <smosker@gmail.com>
2016-04-03 10:09:23 +0300

    Simplify code, add docks

--- excle.py
+++ excle.py
@@ -2,9 +2,14 @@ from tkinter import *
 
 root = Tk()
 
+
 class Application(Frame):
+    """
+    Приложение предназначено для преобразования строк данных в формат
+    подходящий для использования в конструкциях вида 'in (data)' в oracle sql
+    """
     def __init__(self,master):
-        super(Application,self).__init__(master)
+        super(Application, self).__init__(master)
         self.canvas = Canvas(root, borderwidth=0, background="#ffffff")
         self.frame = Frame(self.canvas, background="#ffffff")
         self.vsb = Scrollbar(root, orient="vertical", command=self.canvas.yview)
@@ -13,74 +18,95 @@ class Application(Frame):
         self.canvas.pack(side="left", fill="both", expand=True)
         self.canvas.create_window((4,4), window=self.frame, anchor="nw",tags="self.frame")
 
-        self.frame.bind("<Configure>", self.onFrameConfigure)
-        self.spicok = ''
-        self.erase = []
+        self.frame.bind("<Configure>", self.on_frame_configure)
+
+        self.changed_data = []
+        self.buttons_to_delete = []
         self.create_widgets()
+
+    def on_frame_configure(self, event):
+        """Обеспечивает работу scrollbar"""
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
     def create_widgets(self):
 
-        Label(self.frame,text = 'Вставь значения в поле ниже.',font=("Helvetica", 20) ).grid(row = 0, column = 0, sticky = W)
-        self.values = Text(self.frame,width=25,height = 22, font=("Helvetica", 20), highlightbackground = 'black')
-        self.values.grid(row = 1 ,column = 0)
-        self.bttn = Button(self.frame, text = 'Преобразовать и\n скопировать', command = self.change_and_copy,font=("Helvetica", 20), wraplength = 160)
-        self.bttn.grid(row = 2,column = 0)
+        Label(self.frame, text='Помести значения в поле ниже',
+              font=("Helvetica", 20) ).grid(row = 0, column = 0, sticky = W)
 
-        self.bttn2 =Button(self.frame,text = 'Очистить', command = self.delete,font=("Helvetica", 20))
-        self.bttn2.grid(row = 3,column = 0)
+        self.values = Text(self.frame,width=25,height=22, font=("Helvetica", 20),
+                           highlightbackground = 'black')
+        self.values.grid(row=1, column=0)
 
-        self.exit = Text(self.frame,width=25,height = 22,font=("Helvetica", 20), highlightbackground = 'black')
-        self.exit.grid(row = 1, column = 1)
+        self.bttn = Button(self.frame, text='Преобразовать и\n скопировать',
+                           command=self.change_and_copy, font=("Helvetica", 20),
+                           wraplength=160)
+        self.bttn.grid(row=2, column=0)
 
-    def onFrameConfigure(self, event):
-        '''Reset the scroll region to encompass the inner frame'''
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+        self.bttn2 = Button(self.frame,text='Очистить', command=self.delete, font=("Helvetica", 20))
+        self.bttn2.grid(row=3, column=0)
+
+        self.exit = Text(self.frame,width=25, height=22,font=("Helvetica", 20),
+                         highlightbackground='black')
+        self.exit.grid(row=1, column=1)
 
     def change_and_copy(self):
-        values = self.values.get(0.0,END)
-        values = values.split()
-        self.exit.delete(0.0,END)
-        self.spicok = ''
-        for i in values:
-            value = "'" + i +"',\n"
-            self.spicok +=value
-
-        if len(self.spicok.split()) <=1000:
-            self.exit.insert(0.0,self.spicok[:-2])
-            self.clipboard_clear()
-            self.clipboard_append(self.spicok[:-2])
-
-        elif len(self.spicok.split()) >1000:
-            self.exit.insert(0.0,self.spicok[:-2])
-            self.clipboard_clear()
-            split = self.spicok.split()
-            split1000 = split[:1000]
-            split1000 = "\n".join(split1000)
-            self.clipboard_append(split1000[:-1])
-            for i in range(int(len(split)/1000)):
-                strer = str(i + 1)
-                text = "Скопировать следующую тысячу " + strer
-                self.i = Button(self.frame, text = text, command = lambda i=i: self.copies(i))
-                self.i.grid(row = 4 +i, column = 0)
-                self.erase.append(self.i)
-
-    def copies(self,i):
-        split = self.spicok.split()
-        split = split[(i+1)*1000:(i+2)*1000]
-        split = "\n".join(split)
+        """
+        Основная функция для работы с введенными данными, если входные данные
+        меньше или равны 1000 строк - преобразует их, добавляет в clipboard и
+        выводит на экран. Если число строк более 1000 - разбивает их по 1000
+        и выводит отдельные кнопки для копирования каждой 1000 строк преобразованных
+        данных.
+        входные данные - 123 123 123
+        выходные данные - '123', '123', '123'
+        """
+        values = self.values.get(0.0,END).split()
+        self.exit.delete(0.0, END)
+        self.changed_data.clear()
+        self.changed_data = ["'" + i + "',\n" for i in values]
+        result = ''.join(self.changed_data)[:-2]
+        self.exit.insert(0.0, result)
         self.clipboard_clear()
-        self.clipboard_append(split[:-1])
+
+        if len(self.changed_data) <= 1000:
+            print(self.values.get(0.0,END))
+            self.clipboard_append(result)
+            print(self.clipboard_get())
 
 
+        else:
+            first_thousand = ''.join(self.changed_data[:1000])[:-2]
+            self.clipboard_append(first_thousand)
+
+            for i in range(int(len(self.changed_data)/1000)):
+                text = "Скопировать следующую тысячу " + str(i + 1)
+                self.button_over_thousand = Button(self.frame, text=text, command=lambda i=i: self.copy_next_thousand(i))
+                self.button_over_thousand.grid(row=4 + i, column=0)
+                self.buttons_to_delete.append(self.button_over_thousand)
+
+    def copy_next_thousand(self, i):
+        """
+        Используется дополнительными кнопками в случае, если количество
+        строк данных более 1000, при нажатии на кнопку - копирует следующую
+        1000 строк в clipboard
+        """
+        next_thousand = ''.join(self.changed_data[(i+1)*1000:(i+2)*1000])
+        self.clipboard_clear()
+        self.clipboard_append(next_thousand[:-2])
 
     def delete(self):
-        self.exit.delete(0.0,END)
-        self.values.delete(0.0,END)
-        self.spicok =''
-        for i in self.erase:
+        """
+        Используется для приведения приложения в изначальное состояние
+        удаляет все дополнительные данные, очищает вход/выход
+        """
+        self.exit.delete(0.0, END)
+        self.values.delete(0.0, END)
+        self.changed_data = []
+        for i in self.buttons_to_delete:
             i.destroy()
 
-root.geometry('600x800')
+
+
+root.geometry('620x800')
 app = Application(root)
 root.mainloop()
 
--- test.py
+++ test.py
@@ -1,35 +1,3 @@
-from tkinter import *
+import unittest
 
-root = Tk()
-
-class Application(Frame):
-    def __init__(self,master):
-        super(Application,self).__init__(master)
-        self.place()
-        self.spicok = ''
-        self.erase = []
-        self.create_widgets()
-
-    def create_widgets(self):
-        Label(self,text = 'Вставь значения в поле ниже.',font=("Helvetica", 20) ).place(x=0,y=0)
-        self.values = Text(self,width=25,height = 20, font=("Helvetica", 20), highlightbackground = 'black')
-        self.values.place()
-
-        self.exit = Text(self,width=25,height = 20,font=("Helvetica", 20), highlightbackground = 'black')
-        self.exit.place()
-
-
-        self.bttn = Button(self, text = 'Преобразовать и\n скопировать',font=("Helvetica", 20), wraplength = 160)
-        self.bttn.place()
-
-        self.bttn2 =Button(self,text = 'Очистить',font=("Helvetica", 20))
-        self.bttn2.place()
-
-
-
-
-
-
-root.geometry('600x800')
-app = Application(root)
-root.mainloop()
+class
